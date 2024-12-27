f8215d03925b5149f2262597cbc91dbbf7e45b93
Idhibhat Pankam <marginpankam@gmail.com>
2024-12-27 23:27:30 +0700

    2

--- totally-available-transactions-a/README.md
+++ totally-available-transactions-a/README.md
@@ -1,6 +1,8 @@
 ```bash
 go install .
 
+./maelstrom test -w txn-rw-register --bin ~/go/bin/totally-available-transactions --node-count 1 --time-limit 2 --rate 1000 --concurrency 2n --consistency-models read-uncommitted --availability total
+
 ./maelstrom test -w txn-rw-register --bin ~/go/bin/totally-available-transactions --node-count 1 --time-limit 20 --rate 1000 --concurrency 2n --consistency-models read-uncommitted --availability total
 
 ./maelstrom serve
--- totally-available-transactions-a/main.go
+++ totally-available-transactions-a/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"log"
+	"strconv"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -12,152 +13,68 @@ func main() {
 	node := maelstrom.NewNode()
 	kv := maelstrom.NewSeqKV(node)
 
-	node.Handle("send", func(msg maelstrom.Message) error {
+	node.Handle("txn", func(msg maelstrom.Message) error {
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 
-		ctx := context.Background()
-		key := body["key"].(string)
-		val := int(body["msg"].(float64))
-		var offset int
-
-		for {
-			var oldVal any
-			oldVal, err := kv.Read(ctx, key)
-			if err != nil {
-				kv.CompareAndSwap(ctx, key, []int{}, []int{}, true)
-				oldVal = []int{}
-			}
-
-			currentLogs, err := parseIntSlice(oldVal)
-			if err != nil {
-				return err
-			}
-
-			// check if there has been a concurrent write samlam (oldVal no longer same as current val)
-			newLogs := append(currentLogs, val)
-			err = kv.CompareAndSwap(ctx, key, oldVal, newLogs, false)
-			if err == nil {
-				offset = len(newLogs) - 1
-				break
-			}
-		}
-
-		body["type"] = "send_ok"
-		body["offset"] = offset
-		delete(body, "key")
-		delete(body, "msg")
-
-		return node.Reply(msg, body)
-	})
-
-	node.Handle("poll", func(msg maelstrom.Message) error {
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		offsets, err := parseMapInt(body["offsets"])
-		if err != nil {
+		rawTxn := body["txn"]
+		txnArr, ok := rawTxn.([]interface{})
+		if !ok {
 			return nil
 		}
 
-		msgs := make(map[string][][]int)
-		for key, offset := range offsets {
+		result := []interface{}{}
+		for _, txn := range txnArr {
+			transaction, ok := txn.([]interface{})
+			if !ok {
+				return nil
+			}
+
+			operation, ok := transaction[0].(string)
+			if !ok {
+				return nil
+			}
+
 			ctx := context.Background()
-			var rawLogs any
-			rawLogs, err := kv.Read(ctx, key)
-			if err != nil {
-				return err
-			}
-
-			logs, err := parseIntSlice(rawLogs)
-			if err != nil {
-				return err
-			}
-
-			for i := int(offset); i < len(logs); i++ {
-				msgs[key] = append(msgs[key], []int{i, logs[i]})
-			}
-		}
-
-		body["type"] = "poll_ok"
-		body["msgs"] = msgs
-		delete(body, "offsets")
-
-		return node.Reply(msg, body)
-	})
-
-	node.Handle("commit_offsets", func(msg maelstrom.Message) error {
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		offsets, err := parseMapInt(body["offsets"])
-		if err != nil {
-			return nil
-		}
-
-		for key, offset := range offsets {
-			ctx := context.Background()
-			for {
-				var oldVal any
-				oldVal, err := kv.Read(ctx, key+"_commit")
+			switch operation {
+			case "r":
+				key := transaction[1].(int)
+				val, err := kv.ReadInt(ctx, strconv.Itoa(key))
 				if err != nil {
-					kv.CompareAndSwap(ctx, key+"_commit", -1, -1, true)
-					oldVal = -1
+					result = append(result, []interface{}{"r", key, nil})
+					continue
 				}
 
-				currentOffset, ok := oldVal.(int)
-				if !ok {
-					currentOffset = -1
+				result = append(result, []interface{}{"r", key, val})
+
+			case "w":
+				key := transaction[1].(int)
+				keyStr := strconv.Itoa(key)
+				val := transaction[2].(int)
+
+				for {
+					var oldVal int
+					oldVal, err := kv.ReadInt(ctx, keyStr)
+					if err != nil {
+						kv.CompareAndSwap(ctx, keyStr, 0, 0, true)
+						oldVal = 0
+					}
+
+					// check if there has been a concurrent write samlam (oldVal no longer same as current val)
+					err = kv.CompareAndSwap(ctx, keyStr, oldVal, val, false)
+					if err == nil {
+						break
+					}
 				}
 
-				if int(offset) <= currentOffset {
-					break
-				}
-
-				err = kv.CompareAndSwap(ctx, key+"_commit", oldVal, int(offset), false)
-				if err == nil {
-					break
-				}
+				result = append(result, []interface{}{"w", key, val})
 			}
 		}
 
-		body["type"] = "commit_offsets_ok"
-		delete(body, "offsets")
-
-		return node.Reply(msg, body)
-	})
-
-	node.Handle("list_committed_offsets", func(msg maelstrom.Message) error {
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		keys, err := parseStringArr(body["keys"])
-		if err != nil {
-			return nil
-		}
-
-		offsets := make(map[string]int)
-		for _, key := range keys {
-			ctx := context.Background()
-			offset, err := kv.ReadInt(ctx, key+"_commit")
-			if err != nil {
-				continue
-			}
-
-			offsets[key] = offset
-		}
-
-		body["type"] = "list_committed_offsets_ok"
-		body["offsets"] = offsets
-		delete(body, "keys")
+		body["type"] = "txn_ok"
+		body["txn"] = result
 
 		return node.Reply(msg, body)
 	})
